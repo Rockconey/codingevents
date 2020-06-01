fc8ac75d496ab60dca474146ee6e4e0763b7d835
Shaw Coney <rockconey@gmail.com>
2020-05-31 20:41:50 -0500

    Fragments not working

--- main/java/org/launchcode/codingevents/controllers/EventController.java
+++ main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -3,7 +3,9 @@ package org.launchcode.codingevents.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.lang.reflect.Array;
 import java.util.ArrayList;
@@ -12,19 +14,26 @@ import java.util.ArrayList;
 @RequestMapping("events")
 public class EventController {
 
+    private static ArrayList<String> events = new ArrayList<>();
+
     @GetMapping
     public String displayAllEvents(Model model) {
-        ArrayList<String> events = new ArrayList<>();
-        events.add("Code with Pride");
-        events.add("Strange Loop");
-        events.add("Apple WWDC");
-        events.add("SpringOne Platform");
         model.addAttribute("events", events);
         return "events/index";
     }
 
+    //lives at events/create
+    @GetMapping("create")
+    public String renderCreateEventForm() {
+        return "events/create";
+    }
 
-
+    //lives at /events/create  It can do this because because they handle different types of requests.
+    @PostMapping("create")
+    public String createEvent(@RequestParam String eventName) {
+        events.add(eventName);
+        return "redirect:";
+    }
 
 
 
